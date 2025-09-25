4f14630f160bbb40ab6bfbb4fa4cb8cb65a9fca0
MarianoBaeza <marianobaeza@hotmail.com>
2025-09-25 14:34:59 -0300

    Add enterprises module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnterprisesModule } from './enterprises/enterprises.module';
 
 @Module({
-  imports: [],
+  imports: [EnterprisesModule],
   controllers: [AppController],
   providers: [AppService],
 })
